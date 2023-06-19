cb8f77e1001d841a085ee26e347526c4971b3ed8
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-20 05:12:52 +0700

    [ROUTING] Pemrosesan dan Pembersihan URL pada Kelas App

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,31 @@
 <?php
 
+// Mendefinisikan class App
 class App{
+    // Membuat fungsi konstruktor
     public function __construct() {
-        echo 'OK!';
+        // Memanggil fungsi parseURL() untuk mendapatkan URL yang di-parse
+        $url = $this->parseURL();
+        // Menampilkan var_dump dari variabel $_GET untuk melihat data yang diterima melalui URL
+        var_dump($_GET);
+    }
+
+    // Membuat fungsi parseURL()
+    public function parseURL(){
+        // Memeriksa apakah ada parameter 'url' yang diterima melalui metode GET
+        if(isset($_GET['url'])){
+            // Mengambil nilai 'url' dari variabel $_GET dan menyimpannya dalam variabel $url
+            // Menghilangkan tanda '/' di akhir URL dengan fungsi rtrim()
+            $url = rtrim($_GET['url']);
+
+            // Membersihkan URL dari karakter-karakter yang tidak valid dengan fungsi filter_var()
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            // Memecah URL berdasarkan tanda '/' dengan fungsi explode()
+            $url = explode('/', $url);
+
+            // Mengembalikan nilai $url
+            return $url;
+        }
     }
 }
