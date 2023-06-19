8c5f12126b9efe87470f23c7134b9eafa6440fe5
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-20 01:29:32 +0700

    folder structure init
    "public" folder: Stores files that can be accessed by users.
    "app" folder:
    |-> "core" folder: Contains the core files of the MVC, including routing files.
    |-> "controllers" folder: Stores all controller files.
    |-> "views" folder: Contains the interfaces presented by controllers.
    |-> |-> "home" folder: Stores all views generated by the "home" controller.
    |-> "models" folder: Stores files related to business logic and data processing.

---
+++ app/init.php
@@ -0,0 +1,7 @@
+<?php
+
+//dua file ini adalah class utama pembentuk MVC
+//memanggil file app.php
+require_once 'core/App.php';
+//memanggil file controller.php
+require_once 'core/Controller.php';
