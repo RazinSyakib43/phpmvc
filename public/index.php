7867de015168d80913789a52c493e54be0db58f7
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-20 05:12:52 +0700

    Pemrosesan dan Pembersihan URL pada Kelas App
    Dalam perubahan ini, dilakukan pemrosesan dan pembersihan URL pada kelas App. Pada fungsi konstruktor, dipanggil fungsi parseURL() untuk mendapatkan URL yang akan di-parse. Selanjutnya, menggunakan var_dump($_GET), data yang diterima melalui URL ditampilkan untuk keperluan debugging.
    
    Fungsi parseURL() digunakan untuk memeriksa apakah terdapat parameter 'url' yang diterima melalui metode GET. Jika ada, nilai 'url' diambil dari variabel $_GET dan disimpan dalam variabel $url. Agar URL yang diterima bersih dan valid, dilakukan langkah-langkah berikut:
    
    Tanda '/' di akhir URL dihilangkan menggunakan fungsi rtrim().
    URL dibersihkan dari karakter-karakter yang tidak valid dengan menggunakan fungsi filter_var() dan filter FILTER_SANITIZE_URL.
    URL dipisah menjadi segmen-segmen menggunakan fungsi explode().
    Setelah itu, nilai $url yang telah diproses dikembalikan.
    Dengan perubahan ini, proses pemrosesan dan pembersihan URL pada kelas App telah ditambahkan untuk memastikan bahwa URL yang diterima melalui metode GET telah diproses dan siap digunakan dalam aplikasi.

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@
 //memanggil file init.php untuk menjalankan semua file yang ada di folder app
 require_once '../app/init.php';
 
-//menjalankan class App
+//menjalankan class App untuk menampilkan pesan OK!
 $app = new App;
