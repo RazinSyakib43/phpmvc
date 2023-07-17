737f90ba26c00ab1599c19b1abc9be12b4f4984f
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-17 20:08:24 +0700

    [Controller] update

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,15 @@
 <?php
 
-class About{
+// Mendefinisikan class About
+// Class About ini merupakan child dari class Controller
+class About extends Controller{
     // Membuat method default dengan parameter nama dan pekerjaan
-    public function index($nama = 'Razin', $pekerjaan = 'Mahasiswa'){
-        // Menampilkan pesan dengan parameter nama dan pekerjaan
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan.";
+    public function index($nama = 'Razin', $pekerjaan = 'Mahasiswa', $umur = 21){
+        $this->view('about/index');
     }
 
     // Membuat method page
     public function page(){
-        echo 'about/page';
+        $this->view('about/page');
     }
 }
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,8 +1,10 @@
 <?php
 
-// Mendefinisikan class Home
-class Home{
+// Mendefinisikan class Home 
+// Class Home ini merupakan child dari class Controller
+class Home extends Controller{
     public function index(){
-        echo 'home/index';
+        // Memanggil method view yang ada di dalam class Controller dengan parameter 'home/index'
+        $this->view('home/index');
     }
 }
