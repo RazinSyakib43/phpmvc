9522a3c375201bd68bb25840964f8016f1f0a86b
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2023-07-27 21:48:47 +0700

    [VIEW] Add header and footer component

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,7 +4,10 @@
 // Class Home ini merupakan child dari class Controller
 class Home extends Controller{
     public function index(){
+        $data['judul'] = 'Home';
+        $this->view('templates/header', $data);
         // Memanggil method view yang ada di dalam class Controller dengan parameter 'home/index'
         $this->view('home/index');
+        $this->view('templates/footer');
     }
 }
