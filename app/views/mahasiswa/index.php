850c197aedef7eda96dec86357729171d4365a73
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-30 19:31:32 +0700

    Make form input mahasiswa with modal

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,28 +2,9 @@
     <div class="row">
         <div class="col-6">
             <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
             </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <h3>Daftar Mahasiswa</h3>
 
@@ -35,6 +16,48 @@
                     </li>
                 <?php endforeach; ?>
             </ul>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">Contoh: Muhammad Ibnu</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nim" class="form-label">Nim</label>
+                        <input type="text" class="form-control" id="nim" name="nim" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">Contoh: 1850043</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">Contoh: xxx@gmail.com</div>
+                    </div>
+                    <label for="jurusan" class="form-label">Jurusan</label>
+                    <select class="form-select" aria-label="Default select example" id="jurusan" name="jurusan">
+                        <option value="1">Teknik Informatika</option>
+                        <option value="2">Game Development</option>
+                        <option value="3">Cyber Security</option>
+                    </select>
+                    <div id="emailHelp" class="form-text mb-3">Contoh: Teknik Informatika</div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
